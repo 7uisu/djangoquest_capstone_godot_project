dba235f325aadb4db3abe7deb1a093663f03ca5b
7uisu <hansluisbarrago@gmail.com>
2025-04-06 05:01:05 +0800

    Tweaks on player movement and Added Character walking and idle animations

--- Player/Scripts/player1.gd
+++ Player/Scripts/player1.gd
@@ -1,20 +1,56 @@
-class_name Player1 extends CharacterBody2D
+extends CharacterBody2D
 
-var move_speed : float = 100.0
+const SPEED = 100.0 # Corrected variable name
+var animated_sprite : AnimatedSprite2D
+var animation_player : AnimationPlayer
+var current_dir : String = "down"
 
 func _ready():
-	pass
-	
-func _process( delta ):
-	
+	# Initialize onready variables inside _ready()
+	animated_sprite = $AnimatedSprite2D
+
+func _process(_delta):
 	var direction : Vector2 = Vector2.ZERO
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-	
-	velocity = direction * move_speed
-	
+
+	velocity = direction * SPEED # Corrected variable name
+
+	# Determine current direction for animation
+	if direction.x > 0 and direction.y == 0:
+		current_dir = "right"
+	elif direction.x < 0 and direction.y == 0:
+		current_dir = "left"
+	elif direction.y > 0 and direction.x == 0:
+		current_dir = "down"
+	elif direction.y < 0 and direction.x == 0:
+		current_dir = "up"
+	elif direction.x == 0 and direction.y == 0:
+		pass #keep the last direction
+
 	pass
-	
-func _physics_process( delta ):
+
+func _physics_process(_delta):
 	move_and_slide()
-	
+
+	# Animation control
+	if velocity == Vector2.ZERO:
+		if current_dir == "up":
+			animated_sprite.play("Idle_up")
+		elif current_dir == "down":
+			animated_sprite.play("Idle_down")
+		elif current_dir == "left":
+			animated_sprite.play("Idle_left")
+		elif current_dir == "right":
+			animated_sprite.play("Idle_right")
+		else:
+			animated_sprite.play("Idle_down")  # Default
+	else:
+		if current_dir == "up":
+			animated_sprite.play("walk_up")
+		elif current_dir == "down":
+			animated_sprite.play("walk_down")
+		elif current_dir == "left":
+			animated_sprite.play("walk_left")
+		elif current_dir == "right":
+			animated_sprite.play("walk_right")
