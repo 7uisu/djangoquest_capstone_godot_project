7f039d492b1ae541b598273d2be36199a0123a3f
7uisu <hansluisbarrago@gmail.com>
2025-04-12 00:45:18 +0800

    1. Added Collision and Y-sorting of the Cliffs, Logs and Stubs, Rocks and completed the Collision and Y-sorting of all the Tree's - 2. Completed the new Characters Textures and Animation movements - Both Genders

--- scripts/Player/player.gd
+++ scripts/Player/player.gd
@@ -17,44 +17,39 @@ func _process(_delta):
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 
-	velocity = direction * movement_speed
+	velocity = direction * movement_speed	
+	
+	if direction != Vector2.ZERO:
+		direction = direction.normalized()
+		velocity = direction * movement_speed
 
-	# Determine current direction for animation
-	if direction.x > 0 and direction.y == 0:
-		current_dir = "right"
-	elif direction.x < 0 and direction.y == 0:
-		current_dir = "left"
-	elif direction.y > 0 and direction.x == 0:
-		current_dir = "down"
-	elif direction.y < 0 and direction.x == 0:
-		current_dir = "up"
-	elif direction.x == 0 and direction.y == 0:
-		if current_dir == "":
-			current_dir = "down"  # fallback default direction
+		# Determine current direction for animation
+		if direction.x > 0 and direction.y < 0:
+			current_dir = "up_right"
+		elif direction.x < 0 and direction.y < 0:
+			current_dir = "up_left"
+		elif direction.x > 0 and direction.y > 0:
+			current_dir = "down_right"
+		elif direction.x < 0 and direction.y > 0:
+			current_dir = "down_left"
+		elif direction.x > 0:
+			current_dir = "right"
+		elif direction.x < 0:
+			current_dir = "left"
+		elif direction.y > 0:
+			current_dir = "down"
+		elif direction.y < 0:
+			current_dir = "up"
+	else:
+		velocity = Vector2.ZERO
 
 func _physics_process(_delta):
 	move_and_slide()
 	# Animation control
 	if velocity == Vector2.ZERO:
-		if current_dir == "up":
-			play_idle_animation("up")
-		elif current_dir == "down":
-			play_idle_animation("down")
-		elif current_dir == "left":
-			play_idle_animation("left")
-		elif current_dir == "right":
-			play_idle_animation("right")
-		else:
-			play_idle_animation("down")
+		play_idle_animation(current_dir)
 	else:
-		if current_dir == "up":
-			play_walk_animation("up")
-		elif current_dir == "down":
-			play_walk_animation("down")
-		elif current_dir == "left":
-			play_walk_animation("left")
-		elif current_dir == "right":
-			play_walk_animation("right")
+		play_walk_animation(current_dir)
 
 func play_idle_animation(direction):
 	if character_data.selected_gender == "male":
